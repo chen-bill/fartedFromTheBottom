01f4cf9208c6e327287ddb96ec958bc3a2c37a2e
Bill Chen <Bill@Bills-MacBook-Pro.local>
2016-01-17 05:03:15 -0500

    fix

---
+++ app/www/js/controllers.js
@@ -0,0 +1,138 @@
+angular.module('starter.controllers', [])
+
+.controller('settingsController', function($scope, $cordovaBluetoothLE, $rootScope) {
+  $rootScope.bluetoothAddress;
+  $rootScope.connected = true;
+  $scope.debug = "No Connected";
+  $scope.debug2 = "this is debug text2";
+  $scope.devices = {};
+
+  var params = {request:true};
+  $scope.debug2 = "Initialize : " + JSON.stringify(params);
+  $cordovaBluetoothLE.initialize(params).then(null, initializeError, initializeSuccess);
+
+  function initializeSuccess(obj) {
+    $scope.debug = "Initialize Success : " + JSON.stringify(obj);
+    if (obj.status == "enabled") {
+      $scope.debug = "enabled";
+    }
+    else {
+      $scope.debug = "Unexpected Initialize Status";
+    }
+  }
+
+  function initializeError(obj) {
+    $scope.debug = "Initialize Error : " + JSON.stringify(obj);
+  }
+
+  $scope.startScan = function() {
+    var params = {serviceUuids:[], allowDuplicates: false};
+    $scope.debug = "Start Scan : " + JSON.stringify(params);
+    $cordovaBluetoothLE.startScan(params).then(null, startScanError, startScanSuccess);
+  };
+
+  function startScanSuccess(obj) {
+    $scope.debug = "Start Scan Success : " + JSON.stringify(obj);
+
+    if (obj.status == "scanResult"){
+      addDevice(obj);
+      $scope.debug = 'Scan Result:' + JSON.stringify(obj)
+    } else if (obj.status == "scanStarted") {
+      $scope.debug = "Scan Started";
+    } else {
+      $scope.debug = "Unexpected Start Scan Status";
+    }
+  }
+
+  function startScanError(obj) {
+    console.log("Start Scan Error : " + JSON.stringify(obj));
+    addDevice(obj);
+  }
+
+  $scope.stopScan = function() {
+    $scope.debug = "Stop Scan";
+    $cordovaBluetoothLE.stopScan().then(stopScanSuccess, stopScanError);
+  };
+
+  function stopScanSuccess(obj) {
+    $scope.debug = "Stop Scan Success : " + JSON.stringify(obj);
+
+    if (obj.status == "scanStopped") {
+        $scope.debug = "Scan Stopped";
+    } else {
+        $scope.debug = "Unexpected Stop Scan Status";
+    }
+  }
+
+  function stopScanError(obj) {
+      $scope.debug =  "Stop Scan Error : " + JSON.stringify(obj);
+  }
+
+  $scope.connect = function(obj) {
+    // var address = "98:D3:31:50:20:C8";
+    // $scope.debug2 = address;
+    $scope.debug2 = 'connecting';
+    $scope.debug2 = JSON.stringify(obj.address);
+    var params = {address:obj.address};
+    // var params = {address: address};
+    $scope.debug = "Connecting to : " + JSON.stringify(params);
+    $cordovaBluetoothLE.connect(params).then(null, connectError, connectSuccess);
+  }
+
+  function connectSuccess(obj) {
+    $scope.debug = "Connect Success : " + JSON.stringify(obj);
+
+    if (obj.status == "connected") {
+      $scope.debug = "Connected";
+      $rootScope.bluetoothAddress = obj.address;
+      $rootScope.connected = true;
+    }
+    else if (obj.status == "connecting") {
+      $scope.debug = "Connecting";
+    }
+    else {
+      $scope.debug = "Unexpected Connect Status";
+    }
+  }
+
+  function connectError(obj) {
+    $rootScope.connected = false;
+    console.log("Connect Error : " + JSON.stringify(obj));
+  }
+
+  function addDevice(obj) {
+    if ($scope.devices[obj.address] !== undefined) {
+      return;
+    }
+    obj.services = {};
+    if(obj.name = null){
+      obj.name = "No name";
+    }
+    $scope.devices[obj.address] = obj;
+  }
+
+})
+
+
+.controller('stinkController', function($scope, $cordovaBluetoothLE, $rootScope) {
+  $scope.debug = "Status";
+
+  $scope.stink = function() {
+    var params = {"value":"a","serviceUuid":"180F","characteristicUuid":"2A19","type":"noResponse"};
+    $cordovaBluetoothLE.write(params).then(writeSuccess, writeError);
+  }
+
+  function writeSuccess(obj) {
+    $scope.debug = "Write Success : " + JSON.stringify(obj);
+    if (obj.status == "written") {
+      $scope.debug = "Sprayed";
+    } else {
+      $scope.debug = "Unexpected Write Status";
+    }
+  }
+
+  function writeError(obj) {
+    $scope.debug = "Spray Error : " + JSON.stringify(obj);
+  }
+
+});
